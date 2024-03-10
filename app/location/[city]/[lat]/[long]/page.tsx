9503b0ce8b043d1aa6bab50e13c0fac8015c05a6
cristianworth <cristianweissmantel@gmail.com>
2024-03-10 11:40:38 -0300

    fix: Apollo integration http request fixed

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -1,7 +1,13 @@
 import { MyComponent } from "@/MyComponent";
 import { getClient } from "@/apollo-client";
-import fetchWeatherQuery from "@/fetchWeatherQueries";
-import { ApolloProvider, gql, useQuery } from "@apollo/client";
+import fetchWeatherQuery from "@/graphql/queries/fetchWeatherQueries";
+import {
+  ApolloProvider,
+  gql,
+  useQuery,
+  ApolloClient,
+  InMemoryCache,
+} from "@apollo/client";
 
 type Props = {
   params: {
@@ -12,40 +18,55 @@ type Props = {
 };
 
 async function WeatherPage({ params: { city, lat, long } }: Props) {
-  const { loading, error, data } = useQuery(fetchWeatherQuery, {
-    variables: {
-      current_weather: "true",
-      longitude: long,
-      latitude: lat,
-      timezone: "GMT-3",
+  const client = new ApolloClient({
+    uri: "https://bielefeld.stepzen.net/api/open-meteo/__graphql",
+    headers: {
+      Authorization:
+        "apikey bielefeld::stepzen.io+1000::11c59fe1d7ece26fde59c254679b93a67754d8e2dcad487ae0fa23762550a9f4",
     },
+    cache: new InMemoryCache(),
   });
+  // console.log("the client is working", client);
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Something went wrong...</p>;
-  if (data) return <p>Yay it worked...</p>;
+  const getData = async () => {
+    // Connector: HTTP Error: Bad Request'
+    const { data } = await client.query({
+      query: fetchWeatherQuery,
+      variables: {
+        current_weather: "true",
+        longitude: long,
+        latitude: lat,
+        timezone: "America/Sao_Paulo",
+      },
+    });
 
-  // const client = getClient();
+    const results: Root = data.myQuery;
+    console.log("results", results);
+  };
 
-  // const { data } = await client.query({
-  //   query: fetchWeatherQuery,
-  // variables: {
-  //   current_weather: "true",
-  //   longitude: long,
-  //   latitude: lat,
-  //   timezone: "GMT-3",
-  // },
-  // });
+  const getData2 = () => {
+    // Error: Cannot read properties of undefined (reading 'client')
+    console.clear();
+    const { loading, error, data } = useQuery(fetchWeatherQuery, {
+      variables: {
+        longitude: "-54.24",
+        latitude: "-27.7733",
+        timezone: "GMT",
+      },
+    });
 
-  // const results: Root = data.myQuery;
+    if (loading) console.log("loading", loading);
+    if (error) console.log("error", error);
+    if (data) console.log("data", data);
+  };
 
-  // console.log(results);
-
-  return (
-    <ApolloProvider client={getClient()}>
-      <MyComponent />
-    </ApolloProvider>
-  );
+  // return (
+  //   <ApolloProvider client={client}>
+  //     <MyComponent />
+  //   </ApolloProvider>
+  // );
+  getData();
+  return <div>Hello</div>;
 }
 
 export default WeatherPage;
